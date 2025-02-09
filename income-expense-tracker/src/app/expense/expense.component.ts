d689f8cdf8e752f87f06c62316843b1b7f637c5c
Vedant Mandwe <vedantmandwe5@gmail.com>
2025-02-10 02:06:51 +0530

    Designed Income screen with reactive form to add income and then designed expense screen with reactive form to add expense.

--- income-expense-tracker/src/app/expense/expense.component.ts
+++ income-expense-tracker/src/app/expense/expense.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-expense',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './expense.component.html',
   styleUrl: './expense.component.css'
 })
-export class ExpenseComponent {
+export class ExpenseComponent implements OnInit {
+  totalExpense : number = 500
+  myForm!: FormGroup;
+  isLoading : boolean = false;
+  pastExpenses : any[] = [];
 
+  constructor(private fb : FormBuilder, private router : Router){}
+
+  ngOnInit(): void {
+    this.buildForm();
+    this.pastExpenses = [
+      {
+        title : 'Dining Out',
+        amount : 1000,
+        date : '2025-02-01T00:00:00Z',
+        category : 'Food'
+      },
+      {
+        title : 'Utilities',
+        amount : 500,
+        date : '2025-02-10T00:00:00Z',
+        category : 'Bills'
+      }
+    ]
+  }
+  
+  buildForm(){
+    this.myForm = this.fb.group({
+     title : ['', [Validators.required, Validators.maxLength(20)]],
+     amount : ['', [Validators.required,Validators.pattern(/^\d+$/)]],
+     date : ['', Validators.required],
+     description : ['', Validators.required],
+     category : ['', Validators.required],
+    })
+  }
+
+  allowNumbersOnly(event: KeyboardEvent) {
+    const key = event.key;
+    if (!/^\d$/.test(key)) {
+      event.preventDefault();
+    }
+  }
+  
+  onSave(): void {
+    if (this.myForm.valid) {
+      this.isLoading = true;
+    }
+  }
 }
--- income-expense-tracker/src/app/income/income.component.ts
+++ income-expense-tracker/src/app/income/income.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-income',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './income.component.html',
   styleUrl: './income.component.css'
 })
-export class IncomeComponent {
+export class IncomeComponent implements OnInit{
+totalIncome : number = 5000
+myForm!: FormGroup;
+isLoading : boolean = false;
+pastIncomes : any[] = [];
+constructor(private fb : FormBuilder, private router : Router){}
 
+ngOnInit(): void {
+  this.buildForm();
+  this.pastIncomes = [
+    {
+      title : 'Bonus',
+      amount : 1000,
+      date : '2025-02-01T00:00:00Z',
+      category : 'Income'
+    },
+    {
+      title : 'Freelance Project',
+      amount : 500,
+      date : '2025-02-10T00:00:00Z',
+      category : 'Income'
+    }
+  ]
+}
+
+buildForm(){
+  this.myForm = this.fb.group({
+   title : ['', [Validators.required, Validators.maxLength(20)]],
+   amount : ['', [Validators.required, Validators.minLength(5),
+    Validators.pattern(/^\d+$/)]],
+   date : ['', Validators.required],
+   description : ['', Validators.required],
+   category : ['', Validators.required],
+  })
+}
+allowNumbersOnly(event: KeyboardEvent) {
+  const key = event.key;
+  if (!/^\d$/.test(key)) {
+    event.preventDefault();
+  }
+}
+
+onSave(): void {
+  if (this.myForm.valid) {
+    this.isLoading = true;
+  }
+}
 }
